1f3846ef075ffd0004ad069b337cf792c46a3d64
PrudnikOFF97 <48676900+PrudnikOFF97@users.noreply.github.com>
2019-10-15 15:21:36 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let mini;
+        let maxi;
+        let curr_gues;
+    }
 
     setRange(min, max) {
-
+        this.mini = min;
+        this.maxi = max;
     }
 
     guess() {
-
+        this.curr_gues = this.mini + Math.ceil((this.maxi - this.mini) / 2);
+        return this.curr_gues;
     }
 
     lower() {
-
+        this.maxi = this.curr_gues;
     }
 
     greater() {
-
+        this.mini = this.curr_gues;
     }
 }
 
